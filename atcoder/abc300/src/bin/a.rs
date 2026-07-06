a9887aeaf927125a27654c9e13d168960e78fafc
jazzyvee <jazzyvee917@gmail.com>
2026-07-06 23:37:17 +0900

    solve

--- atcoder/abc300/src/bin/a.rs
+++ atcoder/abc300/src/bin/a.rs
@@ -1,3 +1,25 @@
+use std::io;
+
 fn main() {
-    todo!();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut iter = input.split_whitespace();
+    let _: i32 = iter.next().unwrap().parse().unwrap();
+    let a: i32 = iter.next().unwrap().parse().unwrap();
+    let b: i32 = iter.next().unwrap().parse().unwrap();
+
+    let ans = a + b;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let v: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    for (i, x) in v.iter().enumerate() {
+        if *x == ans {
+            println!("{}", i + 1);
+            return;
+        }
+    }
 }

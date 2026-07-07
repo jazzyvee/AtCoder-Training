4894aef2fc71922550c018d51afe8288f54c5f4d
jazzyvee <jazzyvee917@gmail.com>
2026-07-07 18:15:13 +0900

    partition_pointを使った実装

--- atcoder/abc300/src/bin/d.rs
+++ atcoder/abc300/src/bin/d.rs
@@ -2,7 +2,59 @@ use num::pow;
 use num_integer::Roots;
 use proconio::input;
 
+fn prime_list(n: usize) -> Vec<usize> {
+    let mut res: Vec<usize> = Vec::new();
+    let mut v: Vec<usize> = vec![0; n];
+    for i in 2..n {
+        if v[i] == 1 {
+            continue;
+        }
+        res.push(i);
+        for j in (i..n).step_by(i) {
+            v[j] = 1;
+        }
+    }
+    res
+}
+
 fn main() {
+    // first_try();
+    input! {
+        n:usize
+    }
+    let primes: Vec<usize> = prime_list(3 * 10usize.pow(5));
+    let mut ans = 0;
+    // println!("{}", n.sqrt().sqrt());
+    for (i, &a) in primes.iter().enumerate() {
+        let val1 = a * a;
+        for &b in &primes[i + 1..] {
+            let val2 = val1 * b;
+            if val2 > n / (b + 1) {
+                break;
+            }
+            let c_max = n / val2;
+            let c_max = c_max.sqrt();
+            if c_max <= b {
+                break;
+            }
+            let lo = primes.partition_point(|&x| x <= b);
+            let hi = primes.partition_point(|&x| x <= c_max);
+            ans += hi - lo;
+        }
+    }
+    print!("{}", ans);
+}
+
+fn is_prime(n: usize) -> bool {
+    for i in 2..=n.sqrt() {
+        if n.is_multiple_of(i) {
+            return false;
+        }
+    }
+    true
+}
+
+fn first_try() {
     input! {
         n: usize,
     }
@@ -32,13 +84,4 @@ fn main() {
         }
     }
     print!("{}", ans);
-}
-
-fn is_prime(n: usize) -> bool {
-    for i in 2..=n.sqrt() {
-        if n.is_multiple_of(i) {
-            return false;
-        }
-    }
-    true
 }
